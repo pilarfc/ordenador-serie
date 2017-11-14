028f89ca434b61c35cdd53752c227dbf36d70b50
Pilar Figueroa <pilarfc@MBP-de-Pilar.lan>
2017-11-14 00:51:01 -0600

    fixing redirecting.js

--- redirecting.js
+++ redirecting.js
@@ -6,20 +6,22 @@ casper.test.begin('Test redirect and back to URLs.', 5, function suite(test) {
             test.assertHttpStatus(200);
         })
         .then(function() { // Check page content.
-            test.assertTitle('Crowdbotics: Reinventing software.');
+            test.assertTitle('Crowdbotics: Software Engineering for Everyone.');
             test.assertUrlMatch(/^https:\/\/www.crowdbotics.com\//, 'We are at Crowdbotics home page.');
-        }).then(function() { // Test click /personal and go to Personal page. Then, back to home page.
-            this.click('a[href$=built-with-crowdbotics]'); // #1 Trigger click on a[href$=signup].
-            this.wait(2000, function() { // #2 Wait for 2 seconds and check we are at Personal page.
-                test.assertUrlMatch(/^https:\/\/crowdbotics.github.io\/www-crowdbotics\/signup/, 'We are at Sign Up page.');
+        }).then(function() { // Test click /personal and go to Twitter page. Then, back to home page.
+            this.click('a.twitter'); // #1 Trigger click on a.twitter.
+            this.wait(2000, function() { // #2 Wait for 2 seconds and check we are at Twitter page.
+                test.assertUrlMatch(/^https:\/\/twitter.com\/crowdbotics/, 'We are at Crowdbotics - Twitter account.');
             });
             this.wait(100, function() { // #3 This is important. Use this.wait for synchronized testing or this will be fired after #1 and before #2.
                 casper.back();
             });
-            this.wait(2000, function() { // #4 Wait for 2 seconds and check we are at GitHub home page.
+            this.wait(2000, function() { // #4 Wait for 2 seconds and check we are at Crowdbotics home page.
                 test.assertUrlMatch(/^https:\/\/www.crowdbotics.com\//, 'We are at Crowdbotics home page.');
             });
         }).run(function() {
             test.done();
         });
 });
+
+// To run this in command line: casperjs test testing/redirecting.js
